ae671dd0b76c3ed04f572c6c0458b78add362eb7
EmmaM-W <133997936+EmmaM-W@users.noreply.github.com>
2023-05-23 17:34:44 +0100

    Add getComputerchoice function to main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    x = Math.floor(Math.random()*3);
+    if (x === 0) {
+        return "Rock";
+    }
+    if (x === 1) {
+        return "Paper";
+    }
+    if (x === 2) {
+        return "Scissors";
+    }
+}
