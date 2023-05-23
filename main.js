05264aa925b03b5abb662fc3d452b0cda22876fd
EmmaM-W <133997936+EmmaM-W@users.noreply.github.com>
2023-05-23 17:50:47 +0100

    Add playRound function to main.js

--- main.js
+++ main.js
@@ -10,3 +10,36 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === computerSelection) {
+        return playerSelection + " vs " + computerSelection + "...draw!";
+    }
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            return playerSelection + " vs " + computerSelection + "...you lose!";
+        }
+        if (computerSelection === "Scissors") {
+            return playerSelection + " vs " + computerSelection + "...you win!";
+        }
+    }
+    if (playerSelection === "Paper") {
+        if (computerSelection === "Scissors") {
+            return playerSelection + " vs " + computerSelection + "...you lose!";
+        }
+        if (computerSelection === "Rock") {
+            return playerSelection + " vs " + computerSelection + "...you win!";
+        }
+    }
+    if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            return playerSelection + " vs " + computerSelection + "...you lose!";
+        }
+        if (computerSelection === "Paper") {
+            return playerSelection + " vs " + computerSelection + "...you win!";
+        }
+    }
+
+}
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
